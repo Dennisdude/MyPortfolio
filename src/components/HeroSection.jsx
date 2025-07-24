76091de8cddf08bd30224259586e70d8e350a266
Dennis <dennisdeifel@gmail.com>
2025-07-24 23:38:15 +0200

    Update HeroSection component with full description of education

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ export const HeroSection = () => {
                     </h1>
 
                     <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-                        Im a student @HHN, ...
+                        Im a student at Hochschule Heilbronn - Software Engineering Bachelor
                     </p>
 
                     <div className="pt-4 opacity-0 animate-fade-in-delay-4">
