3be7dd37bcc264294249d6301eb135b52e004990
Dennis Deifel <dennisdeifel@gmail.com>
2025-12-09 15:55:04 +0100

    Comment out ProjectSection in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export const Home = () => {
                 <HeroSection />
                 <About />
                 <SkillsSection />
-                <ProjectSection />
+                {/*<ProjectSection />*/}
                 <ContactSection />
             </main>
 
