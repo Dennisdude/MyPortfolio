693de4ccc5b031cf08128519bd38ab9769787388
Dennis Deifel <dennisdeifel@gmail.com>
2025-12-09 16:30:32 +0100

    Add DayBackground component and refactor ThemeToggle in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,44 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
+import { DayBackground } from "../components/DayBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection"
 import { About } from "../components/About"
 import { SkillsSection } from "../components/SkillsSection"
 import { ProjectSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
+    const [isDarkMode, setIsDarkMode] = useState(() => {
+        const storedTheme = localStorage.getItem("theme");
+        if (!storedTheme) return true; // Default zu Dark Mode
+        return storedTheme === "dark";
+    });
+
+    useEffect(() => {
+        const root = document.documentElement;
+
+        if (isDarkMode) {
+            root.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            root.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+        }
+    }, [isDarkMode]);
+
+    const toggleTheme = () => {
+        setIsDarkMode((prevMode) => !prevMode);
+    }
+
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             {/* Theme Toggle */}
-            <ThemeToggle />
+            <ThemeToggle isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
 
             {/* Background Effects*/}
-            <StarBackground />
+            {isDarkMode ? <StarBackground /> : <DayBackground />}
             
             {/* Navbar */}
             <Navbar />
